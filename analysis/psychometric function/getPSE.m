3b987bd2e964b0684bee757724a05a524a486801
Xiuyun Wu <xiuyunwu5@gmail.com>
2017-10-25 01:10:48 -0700

    fitting PSE and plotting pilot

--- analysis/psychometric function/getPSE.m
+++ analysis/psychometric function/getPSE.m
@@ -1,40 +1,59 @@
 % function getPSE
-% Calculate psychometric function for each flash onset, initial
-% direction merged
+% Calculate psychometric function for each flash onset, including
+% experiment and baseline data
 % Then save the data, and draw the plots
 
-% 10/13/2017, Xiuyun Wu
+% 10/24/2017, Xiuyun Wu
 
 % some (maybe) useful codes from the past...
 % arr = find(all(tabdata{:, 3:6}==cont(:,1:4),2));
 % M1 = accumarray(subs, LinTh, [], @mean); % mean for linear threshold contrast
 clear all; close all; clc
+folder = pwd;
 
 % basic setting
-names = {'XW2'};
+names = {'XW2' 'NI'};
 conditionNames = {'flashOnset', 'flashDisplaceLeft', 'initialDirection'}; % which conditions are different
 merged = 0; % whether initial direction is merged; 1=merged
 roundN = -4; % keep how many numbers after the point when rounding and matching...; -1 for the initial pilot
+myfittype = fittype('a*(1-exp(-s*(x-ic)))', 'independent', 'x');
+% myfittype = fittype('a*(1-exp(-(x/lambda).^(k)))', 'independent', 'x');
 
-% load raw data collapsed
-cd ..
-load(['dataRaw_all', num2str(size(names, 2))])
-% back into the folder
-cd('psychometric function')
+% % load raw data collapsed
+% cd ..
+% load(['dataRaw_all', num2str(size(names, 2))])
+% load(['dataRawBase_all', num2str(size(names, 2))])
+% % back into the folder
+% cd(folder)
 
-dataPMFall = table();
-for ii = 1:size(names, 2)
-    % initialize data points for the psychometric function
-    dataPMF = table();
+dataPMFall = table(); % experiment
+dataPMFbaseAll = table(); % baseline
+for ii = 1:1%size(names, 2)
+    % load raw data for each participant
+    cd ..
+    load(['dataRaw_', names{ii}])
+    load(['dataRawBase_', names{ii}])
+    % back into the folder
+    cd(folder)
     
-    % locate data for each individual
-    idx = find(strcmp(dataRawAll.sub, names{ii}));
-    dataRaw = dataRawAll(idx,:);
+    % initialize data points for the psychometric function
+    dataPMF = table(); % experiment
+    dataPMFbase = table(); % baseline
+    
+    %     % locate data for each individual
+    %     idx = find(strcmp(dataRawAll.sub, names{ii}));
+    %     dataRaw = dataRawAll(idx,:); % experiment
+    %     idx = find(strcmp(dataRawBaseAll.sub, names{ii}));
+    %     dataRawBase = dataRawBaseAll(idx,:); % baseline
     % get the levels of each condition
     for jj = 1:size(conditionNames, 2)
-        eval(['cons{jj} = unique(roundn(dataRaw.', conditionNames{jj}, ', roundN));']) 
+        eval(['cons{jj} = unique(roundn(dataRaw.', conditionNames{jj}, ', roundN));']) % experiment
+        eval(['consBase{jj} = unique(roundn(dataRawBase.', conditionNames{jj}, ', roundN));']) % baseline
     end
+    xFit = min(unique(dataRaw.flashDisplaceLeft)):0.01:max(unique(dataRaw.flashDisplaceLeft));
+    xFitBase = min(unique(dataRawBase.flashDisplaceLeft)):0.01:max(unique(dataRawBase.flashDisplaceLeft));
     
+    %% Experiment data, flash onset is important
     tempI = 1; % index to add into the table of dataPMF
     
     onsetIdx = find(strcmp(conditionNames, 'flashOnset'));
@@ -68,61 +87,158 @@ for ii = 1:size(names, 2)
             tempI = tempI+1;
         end
         
-        if merged==1
-            %         draw plots for each flash onset, initial direction merged
-            idx = find(dataPMF.flashOnset==cons{onsetIdx}(jj));
-            dataPlot = dataPMF(idx, :);
-            figure
-            scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
-            ylim([0, 1])
-            xlabel('Flash Displacement (left-right, dva)')
-            ylabel('Percentage of perceived left to be lower')
-            title(['flash onset ', num2str(12*cons{onsetIdx}(jj)), ' s'])
-            saveas(gca, [names{ii}, '_merged_onset', num2str(12*cons{onsetIdx}(jj)), '.pdf'])
+        % draw plots
+        if merged==1 % modify later
+            %             % draw plots for each flash onset, initial direction merged
+            %             idx = find(dataPMF.flashOnset==cons{onsetIdx}(jj));
+            %             dataPlot = dataPMF(idx, :);
+            %
+            %             figure
+            %             scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
+            %             ylim([0, 1])
+            %             xlabel('Flash Displacement (left-right, dva)')
+            %             ylabel('Percentage of following current motion')
+            %             title(['flash onset ', num2str(12*cons{onsetIdx}(jj)), ' s'])
+            %             saveas(gca, [names{ii}, '_merged_onset', num2str(12*cons{onsetIdx}(jj)), '.pdf'])
         else
             % draw plots for each flash onset, initial direction not merged
             figure
-            
-            subplot(1, 2, 1)            
+            box off
+            subplot(1, 2, 1) % initial clockwise
             idx = find(dataPMF.flashOnset==cons{onsetIdx}(jj) & dataPMF.initialDirection==1);
             dataPlot = dataPMF(idx, :);
+            [fitObj{jj, 1} gof{jj, 1}]= fit(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, myfittype);
             
+            % raw data
             scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
+            hold on
+            % fitted line
+            plot(xFit, fitObj{jj, 1}.a*(1-exp(-fitObj{jj, 1}.s*(xFit-fitObj{jj, 1}.ic))), '-k')
+%             plot(xFit, fitObj{jj, 1}.a*(1-exp(-(xFit/fitObj{jj, 1}.lambda).^fitObj{jj, 1}.k)), '-k')
+            % get the PSE
+            PSE(jj, 1) = log(1-0.5/fitObj{jj, 1}.a)/(-fitObj{jj, 1}.s)+fitObj{jj, 1}.ic;
+            
             ylim([0, 1])
             xlabel('Flash Displacement (left-right, dva)')
             ylabel('Percentage of perceived left to be lower')
-            title(['initial clockwise'])
+            title([num2str(cons{onsetIdx}(jj)), 's initial clockwise'])
             
-            subplot(1, 2, 2)
+            subplot(1, 2, 2) % initial counterclockwise
             idx = find(dataPMF.flashOnset==cons{onsetIdx}(jj) & dataPMF.initialDirection==-1);
             dataPlot = dataPMF(idx, :);
+            [fitObj{jj, 2} gof{jj, 2}]= fit(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, myfittype);
             
+            % raw data
             scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
+            hold on
+            % fitted line
+            plot(xFit, fitObj{jj, 2}.a*(1-exp(-fitObj{jj, 2}.s*(xFit-fitObj{jj, 2}.ic))), '-k')
+% plot(xFit, fitObj{jj, 2}.a*(1-exp(-(xFit/fitObj{jj, 2}.lambda).^fitObj{jj, 2}.k)), '-k')
+
+            % get the PSE
+            PSE(jj, 2) = log(1-0.5/fitObj{jj, 2}.a)/(-fitObj{jj, 2}.s)+fitObj{jj, 2}.ic;
+            
             ylim([0, 1])
             xlabel('Flash Displacement (left-right, dva)')
             ylabel('Percentage of perceived left to be lower')
-            title(['initial counterclockwise'])
+            title([num2str(cons{onsetIdx}(jj)), 's initial counterclockwise'])
             
-            saveas(gca, [names{ii}, '_notMerged_onset', num2str(cons{onsetIdx}(jj)), '.pdf'])
+            saveas(gca, [names{ii}, '_notMerged_onset', num2str(cons{onsetIdx}(jj)), '_fit.pdf'])
         end
     end
-    if merged==1
-        save(['dataPMFmerged_', names{ii}], 'dataPMF')
-    else
-        save(['dataPMFnotMerged_', names{ii}], 'dataPMF')
+    
+    % plot for the fitted PSE
+    figure
+    box off
+    plot(cons{onsetIdx}, PSE(:, 1), '-b')
+    hold on
+    plot(cons{onsetIdx}, PSE(:, 2), '-r')
+    legend({'Initial clockwise', 'Initial counterclockwise'}, 'box', 'off')
+    xlabel('Flash Onset (s)')
+    ylabel('Point of subjective equality, left-right')
+    saveas(gca, [names{ii}, '_notMerged_PSE.pdf'])
+    
+    %% Baseline data, flash onset was not important and merged
+    tempI = 1; % index to add into the table of dataPMFbase
+    
+    comb = consBase{2};
+    for tt = 1:size(comb, 1)
+        dataPMFbase.sub(tempI, 1) = names(ii);
+        idxAll = 1:size(dataRawBase, 1);
+        for aa = 1:size(comb, 2)
+            eval(['dataPMFbase.', conditionNames{aa+1}, '(tempI, 1) = ', num2str(comb(tt, aa)), ';'])
+            eval(['idx = find(roundn(dataRawBase.', conditionNames{aa+1}, ', roundN)==', num2str(comb(tt, aa)), ');'])
+            idxAll = intersect(idxAll, idx);
+        end
+        if merged==1
+            idx = find(dataRawBase.perceivedLower(idxAll)==1);
+        else
+            idx = find(dataRawBase.perceivedLowerNotMerged(idxAll)==1);
+        end
+        dataPMFbase.percentLeftLower(tempI, 1) = length(idx)/length(idxAll);
+        
+        tempI = tempI+1;
     end
     
-    % collapse data
-    if ii==1
-        dataPMFall = dataPMF;
+    % draw plots
+    if merged==1 % modify later
+        %             % draw plots for each flash onset, initial direction merged
+        %             idx = find(dataPMF.flashOnset==cons{onsetIdx}(jj));
+        %             dataPlot = dataPMF(idx, :);
+        %
+        %             figure
+        %             scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
+        %             ylim([0, 1])
+        %             xlabel('Flash Displacement (left-right, dva)')
+        %             ylabel('Percentage of following current motion')
+        %             title(['flash onset ', num2str(12*cons{onsetIdx}(jj)), ' s'])
+        %             saveas(gca, [names{ii}, '_merged_onset', num2str(12*cons{onsetIdx}(jj)), '.pdf'])
     else
-        dataPMFall = [dataPMFall; dataPMF];
+        % draw plots for each flash onset, initial direction not merged
+        figure
+        box off
+        dataPlot = dataPMFbase;
+        [fitObjBase gofBase]= fit(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, myfittype);
+        
+        % raw data
+        scatter(dataPlot.flashDisplaceLeft, dataPlot.percentLeftLower, 'filled');
+        hold on
+        % fitted line
+        plot(xFit, fitObjBase.a*(1-exp(-fitObjBase.s*(xFit-fitObjBase.ic))), '-k')
+%         plot(xFit, fitObjBase.a*(1-exp(-(xFit/fitObjBase.lambda).^fitObjBase.k)), '-k')
+        % get the PSE
+        PSEbase = log(1-0.5/fitObjBase.a)/(-fitObjBase.s)+fitObjBase.ic;
+        
+        ylim([0, 1])
+        xlabel('Flash Displacement (left-right, dva)')
+        ylabel('Percentage of perceived left to be lower')
+        title(['PSE=', num2str(PSEbase(1))])
+        
+        saveas(gca, [names{ii}, '_notMerged_baseline_fit.pdf'])
     end
+    
+    if merged==1
+        save(['dataPMFmerged_', names{ii}], 'dataPMF', 'fitObj', 'gof', 'PSE') % experiment
+        save(['dataPMFbaseMerged_', names{ii}], 'dataPMFbase', 'fitObjBase', 'gofBase', 'PSEbase') % baseline
+    else
+        save(['dataPMFnotMerged_', names{ii}], 'dataPMF', 'fitObj', 'gof', 'PSE') % experiment
+        save(['dataPMFbaseNotMerged_', names{ii}], 'dataPMFbase', 'fitObjBase', 'gofBase', 'PSEbase') % baseline
+    end
+    %
+    %     % collapse data
+    %     if ii==1
+    %         dataPMFall = dataPMF; % experiment
+    %         dataPMFbaseAll = dataPMFbase; % baseline
+    %     else
+    %         dataPMFall = [dataPMFall; dataPMF]; % experiment
+    %         dataPMFbaseAll = [dataPMFbaseAll; dataPMFbase];  % baseline
+    %     end
 end
-if merged==1
-    save(['dataPMFmerged_all', num2str(size(names, 2))], 'dataPMFall')
-else
-    save(['dataPMFnotMerged_all', num2str(size(names, 2))], 'dataPMFall')
-end
-
+% % collapsed data
+% if merged==1
+%     save(['dataPMFmerged_all', num2str(size(names, 2))], 'dataPMFall') % experiment
+%     save(['dataPMFbaseMerged_all', num2str(size(names, 2))], 'dataPMFbaseAll') % baseline
+% else
+%     save(['dataPMFnotMerged_all', num2str(size(names, 2))], 'dataPMFall') % experiment
+%     save(['dataPMFbaseNotMerged_all', num2str(size(names, 2))], 'dataPMFbaseAll') % baseline
 % end
--- analysis/sortRawData.m
+++ analysis/sortRawData.m
@@ -1,29 +1,32 @@
 % function sortRawData
-% Sorting raw data into long format
+% Sorting raw data into long format, baseline in separate files
 % Merged different initial directions into clockwise: 
 % clockwise right side = counterclockwise left side.
 % Under clockwise direction, positive=perceived left lower 
 % (initialDirection*reportStyle*choice)
 
-% 10/13/2017 Xiuyun Wu
+% 10/24/2017 Xiuyun Wu
 clear all; close all; clc
 
 % basic setting
-names = {'XW2'};
+names = {'XW2' 'NI'};
+folder = pwd;
 
 dataRawAll = table();
+dataRawBaseAll = table();
 for ii = 1:size(names, 2)
-    % read all raw data
+    % Read all raw data     
     cd ..
-    cd(['data\', names{ii}])
+    % read Experiment data
+    cd(['data\', names{ii}]) 
     % get the filenames to load
     fileResp = dir('response*.mat');
     fileResp = struct2cell(fileResp);
-    % load raw data into dataRawAll
+    % load raw data into dataRaw
     dataRaw = table();
     for jj = 1:size(fileResp, 2)
         load(fileResp{1, jj})
-%         % for initial pilots... still adjusting experimental codes...
+%         % ONLY for the initial pilot... still adjusting experimental codes...
 %         resp = resp{1, size(resp, 2)};
         
         % regular processing below, first putting all data together
@@ -33,39 +36,61 @@ for ii = 1:size(names, 2)
             dataRaw = [dataRaw; resp];
         end
     end
+    % read Baseline data
+    cd(['baseline']) 
+    % get the filenames to load
+    fileResp = dir('response*.mat');
+    fileResp = struct2cell(fileResp);
+    % load raw data into dataRawBase
+    dataRawBase = table();
+    for jj = 1:size(fileResp, 2)
+        load(fileResp{1, jj})
+        
+        % regular processing below, first putting all data together
+        if jj==1
+            dataRawBase = resp;
+        else
+            dataRawBase = [dataRawBase; resp];
+        end
+    end
     
-    % go to the analysis file to save the processed data
-    cd ..
-    cd ..
-    cd('analysis')
+    % Go to the analysis file to save the processed data
+    cd(folder)
     % delete invalid trials
     dataRaw(dataRaw.choice==0, :) = [];
-    dataRaw.sub = mat2cell(repmat(names{ii}, size(dataRaw, 1), 1), ones(size(dataRaw, 1), 1), length(names{ii}));
+    dataRaw.sub = mat2cell(repmat(names{ii}, size(dataRaw, 1), 1), ones(size(dataRaw, 1), 1), length(names{ii})); % experiment
+    dataRawBase(dataRawBase.choice==0, :) = [];
+    dataRawBase.sub = mat2cell(repmat(names{ii}, size(dataRawBase, 1), 1), ones(size(dataRawBase, 1), 1), length(names{ii})); % baseline
     
 %     % ONLY for the initial pilot...leftArrow=1, rightArrow=2; reportStyle = -1;
 %     dataRaw.reportStyle = repmat([-1], size(dataRaw, 1), 1);
 %     dataRaw.flashDisplaceLeft = dataRaw.flashDisplaceLeft*-1;
 %     dataRaw.choice(dataRaw.choice==1) = -1; % choose left
 %     dataRaw.choice(dataRaw.choice==2) = 1; % choose right
-    %
+    
     dataRaw.perceivedLower = dataRaw.initialDirection.*dataRaw.reportStyle.*dataRaw.choice; % perception of the side being lower
     % 1: left lower; -1: right lower
     % this is merged direction, all initial direction assumed to be
     % clockwise
+    dataRawBase.perceivedLower = dataRawBase.initialDirection.*dataRawBase.reportStyle.*dataRawBase.choice; 
     
     dataRaw.perceivedLowerNotMerged = dataRaw.reportStyle.*dataRaw.choice; % perception of the side being lower
     % initial direction not merged, 1: left lower; -1: right lower
+    dataRawBase.perceivedLowerNotMerged = dataRawBase.reportStyle.*dataRawBase.choice; 
     
     % save data with each trials for each participant
-    save(['dataRaw_', names{ii}], 'dataRaw')
+    save(['dataRaw_', names{ii}], 'dataRaw') % experiment    
+    save(['dataRawBase_', names{ii}], 'dataRawBase') % baseline
     
     % collapse all data
     if ii==1
-        dataRawAll = dataRaw;
+        dataRawAll = dataRaw; % experiment
+        dataRawBaseAll = dataRawBase; % baseline
     else
-        dataRawAll = [dataRawAll; dataRaw];
+        dataRawAll = [dataRawAll; dataRaw]; % experiment
+        dataRawBaseAll = [dataRawBaseAll; dataRawBase]; % baseline
     end
+    
 end
-
-save(['dataRaw_all', num2str(size(names, 2))], 'dataRawAll')
-% end
+% save(['dataRaw_all', num2str(size(names, 2))], 'dataRawAll')
+% save(['dataRawBase_all', num2str(size(names, 2))], 'dataRawBaseAll')
